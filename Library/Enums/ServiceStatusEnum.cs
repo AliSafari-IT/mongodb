6a22157cea629849a2d52f8aaf265aa66e1ccb20
asafarim <asafarim.it@gmail.com>
2024-02-18 05:35:43 +0100

    update  tools

--- Library/Enums/ServiceStatusEnum.cs
+++ Library/Enums/ServiceStatusEnum.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Commons.Enums
+namespace Library.Enums
 {
     public enum ServiceStatusEnum
     {
--- Library/Tools/GraphQLQuery.cs
+++ Library/Tools/GraphQLQuery.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Commons
+namespace Library.Tools
 {
     public class GraphQLQuery
     {
